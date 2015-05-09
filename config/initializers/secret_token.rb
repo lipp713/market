f76b3eead63a4176aff11c1e39597ef4f81997f9
lipp713 <lipp713@gmail.com>
2015-05-09 17:29:48 +0100

    Created Martet application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Market::Application.config.secret_key_base = '9981fb5480e99f9eaf9eebf932cdf30d1ac2359872f951dbd7d6875a9dee81ca11cfeb3d92c550c1fbf4957a3d81c4579e7b5f15eb0f02586aad7b4aeda03152'
