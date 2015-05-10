fe3f53db24aa2497872e73092812c2fe63bf2b8b
lipp713 <lipp713@gmail.com>
2015-05-10 20:14:04 +0100

    Configured AWS with Paperclip

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,10 @@ class Product < ActiveRecord::Base
 belongs_to :user
 has_attached_file :image
 
+validates_attachment_content_type :image, 
+content_type:  /^image\/(png|gif|jpeg)/,
+message: "Only images allowed"
+ 
+validates :image, attachment_presence: true
+
 end
